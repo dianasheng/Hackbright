1f74b4e7924e8783acee122bf2564cd4bf27c1ef
diana sheng <dianasheng@gmail.com>
2016-01-15 16:43:13 -0800

    Bill Calculator project

---
+++ billCalculator.py
@@ -0,0 +1,11 @@
+# This is a comment and it not read by the interpreter
+# These are useful for describing difficult code or adding reminders.
+# TODO - fix this code :D
+
+float(bill) = raw_input("How much was your bill?")
+
+tip = bill * .18
+
+total_bill = bill + tip
+
+print "The tip is %f and the total bill is %f ." % (tip, total_bill)
