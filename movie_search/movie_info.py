2f464407ddec90ae7ee4fa0c1f4cccd4ecba65c6
diana sheng <dianasheng@gmail.com>
2016-06-15 07:11:25 -0700

    Visual GUI Project

---
+++ movie_search/movie_info.py
@@ -0,0 +1,9 @@
+class MovieInfo(object):
+    def __init__(self, director='', release_year ='', title='', actor_1='', actor_2='', location=''):
+        self.director = director
+        self.release_year = release_year
+        self.title = title
+        self.actor_1 = actor_1
+        self.actor_2 = actor_2
+        self.location = location
+
