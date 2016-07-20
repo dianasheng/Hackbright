1c82a6f558f0cbf97c689c32d5329f132b9cd52b
diana sheng <dianasheng@gmail.com>
2016-07-20 06:14:31 -0700

    codeappetizer_github

--- CodeAppetizer_GitHub/billCalculator.py
+++ CodeAppetizer_GitHub/billCalculator.py
@@ -1,11 +1,7 @@
 # This is a comment and it not read by the interpreter
 # These are useful for describing difficult code or adding reminders.
-# TODO - fix this code :D
-# (Part 1): Instead of using the hard coded ".18",
-# Ask the user to input the percentage of tip they want to give. 
-# Save the input to a variable. 
-# Use the variable to calculate the tip.
-# (Part 2): Fix any bugs and make it work!
+# TODO - Read this code and fix it.
+# (Part 1): Fix any bugs and make it work!
 
 bill = raw_input("How much was your bill?")
 
