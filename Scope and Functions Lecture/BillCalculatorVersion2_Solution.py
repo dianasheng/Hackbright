2754ef7647059f648453ef825c488362309b6f73
diana sheng <dianasheng@gmail.com>
2016-07-29 06:17:20 -0700

    Optional Parameter exercise

--- Scope and Functions Lecture/BillCalculatorVersion2_Solution.py
+++ Scope and Functions Lecture/BillCalculatorVersion2_Solution.py
@@ -8,35 +8,37 @@ total_bill = 0
 split_bill = 0
 
 
-def calculate_bill(bill_amount, tip_percentage = 18, split_number = 1):
+def calculate_bill(original_bill_amount, tip_percentage = 18, split_number = 1):
     global tip_amount
-    tip_amount = bill_amount * tip_percentage * .01
+    tip_amount = original_bill_amount * tip_percentage * .01
     global total_bill
-    total_bill = bill_amount + tip_amount
+    total_bill = original_bill_amount + tip_amount
     global split_bill
     split_bill = total_bill / split_number
 
+
 def main():
-    calculate_bill(100)
-    print "original bill is 100"
+    original_bill = 100
+    calculate_bill(original_bill)
+    print "original bill is", original_bill
     print "tip amount", tip_amount
     print "total bill", total_bill
     print "split amount", split_bill
 
-    calculate_bill(100, tip_percentage=20)
-    print "original bill is 100"
+    calculate_bill(original_bill, tip_percentage=20)
+    print "original bill is", original_bill
     print "tip amount", tip_amount
     print "total bill", total_bill
     print "split amount", split_bill
 
-    calculate_bill(100, split_number=4)
-    print "original bill is 100"
+    calculate_bill(original_bill, split_number=4)
+    print "original bill is", original_bill
     print "tip amount", tip_amount
     print "total bill", total_bill
     print "split amount", split_bill
 
-    calculate_bill(100, 20, 3)
-    print "original bill is 100"
+    calculate_bill(original_bill, 20, 3)
+    print "original bill is", original_bill
     print "tip amount", tip_amount
     print "total bill", total_bill
     print "split amount", split_bill
