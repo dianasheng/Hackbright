5026676668afd7d6903a9107ed7b23d9ff9578a1
diana sheng <dianasheng@gmail.com>
2016-08-09 13:24:35 -0700

    Skeleton Code Demo

---
+++ CodeWorkshop_Dictionaries_Shopping List/shopping_list_skeleton.py
@@ -0,0 +1,64 @@
+'''
+
+Code Workshop - Dictionary Shopping list.
+This program will do the following.
+0 - Main Menu
+1 - Show all lists.
+2 - Show a specific list. 
+3 - Add a new shopping list.
+4 - Add an item to a shopping list.
+5 - Remove an item from a shopping list.
+6 - Remove a list by nickname.
+7 - Exit when you are done.
+
+'''
+
+# LET'S WRITE SOME SKELETON CODE!
+
+
+def menu_prompt():
+    # todo: display menu and get choice using raw_input from user.
+    pass
+
+
+def show_all_lists():
+    # todo:
+    pass
+
+
+def show_sorted_list(key):
+    # todo:
+    pass
+
+
+def add_new_list(key):
+    # add a new list with key as name of the list
+    # make sure a list with this name doesn't already exist
+    # deal with uppercase/lowercase
+    pass
+
+
+def add_item_to_list(key, item):
+    # add an item to an existing list
+    # make sure the item doesn't already exist
+    # deal with uppercase/lowercase
+    pass
+
+
+def remove_item_from_list(key, item):
+    # todo:
+    pass
+
+
+def remove_list(key):
+    # todo:
+    pass
+
+
+def main():
+    # this is where the core logic will be
+    # start by calling the menu prompt to return the user's choice
+    pass
+
+if __name__ == "__main__":
+    main()
--- WhileLoops-ShoppingList/shopping_list_while_loop.py
+++ WhileLoops-ShoppingList/shopping_list_while_loop.py
@@ -70,7 +70,6 @@ def main():
 
 
 if __name__ == '__main__':
-    print __name__
     main()
 
 
