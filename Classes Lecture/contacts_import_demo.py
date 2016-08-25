55c20e6d54148942084467f4d4a34ba2a554aa69
diana sheng <dianasheng@gmail.com>
2016-08-24 17:52:37 -0700

    Classes Lecture Demo and Solutions

--- Classes Lecture/contacts_import_demo.py
+++ Classes Lecture/contacts_import_demo.py
@@ -2,10 +2,13 @@ import contact
 
 
 def main():
-
+    # create an instance of Contact
     new_contact = contact.Contact(first_name="Diana", last_name="Smith", mobile_phone="555-555-5555")
+
+    # change an attribute
     new_contact.last_name = "Banana"
 
+    # print the attributes
     print new_contact.mobile_phone
     print new_contact.first_name
 
